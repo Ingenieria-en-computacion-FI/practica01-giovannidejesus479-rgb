5a0c0a55d371ef482d9505c7d4f4706ea63e25f5
giovannidejesus479-rgb <giovannidejesus479@gmail.com>
2026-02-24 22:39:19 -0600

    Update ej6.c

--- soluciones/ej6.c
+++ soluciones/ej6.c
@@ -1,6 +1,13 @@
+#include<stdio.h>
 
-#include <stdio.h>
 int main(){
-    // Resolver ejercicio 6
-    return 0;
+	
+	int m[2][4];
+	
+	for(int i=0; i<2; i++){
+		for(int j=0; j<4; j++){
+			printf("&m[%d][%d] = %p\n", i, j, &m[i][j]);
+		}
+	}
+	return 0;
 }
