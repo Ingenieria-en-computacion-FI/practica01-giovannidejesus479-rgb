f8b5411b14a469b016d5cbd99b18b679e6a85b0d
giovannidejesus479-rgb <giovannidejesus479@gmail.com>
2026-02-24 22:34:48 -0600

    Update ej3.c

--- soluciones/ej3.c
+++ soluciones/ej3.c
@@ -1,6 +1,22 @@
+#include<stdio.h>
 
-#include <stdio.h>
 int main(){
-    // Resolver ejercicio 3
-    return 0;
+	
+	int a[3];
+	int b[3];
+	int c[3];
+	
+	for( int i=0; i<3; i++){
+		printf("&a[%d] = %p\n", i, &a[i]);
+	}
+	printf("\n");
+	
+	for (int i=0; i<3; i++){
+		printf("&b[%d] = %p\n", i, &b[i]);
+	}
+	printf("\n");
+	for (int i=0; i<3; i++){
+		printf("&c[%d] = %p\n", i, &c[i]);
+	}
+	return 0;
 }
