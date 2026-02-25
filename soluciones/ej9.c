80ede1225c4c710954fcaca6eb0a9f7740b95b00
giovannidejesus479-rgb <giovannidejesus479@gmail.com>
2026-02-24 22:41:22 -0600

    Update ej9.c

--- soluciones/ej9.c
+++ soluciones/ej9.c
@@ -1,6 +1,14 @@
+#include<stdio.h>
 
-#include <stdio.h>
 int main(){
-    // Resolver ejercicio 9
-    return 0;
+	
+	int a[5], i=0;
+	
+	printf("&a[%d] = %p\n", i-1, &a[i-1]);
+	printf("&a[%d] = %p\n", i+5, &a[i+5]);
+	
+	for (int i=0; i<5; i++){
+		printf("&a[%d] = %p\n", i, &a[i]);
+	}
+	return 0;
 }
